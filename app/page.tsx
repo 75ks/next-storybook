10c3ca503fc8237d3f992e8fff75991c0cfa2d64
75ks <kenta.s705@gmail.com>
2023-08-05 02:49:12 +0000

    ボタン部品のインポートパス修正、 onClickを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import { CustomButton } from "./components/CustomButton";
+"use client";
+
+import { CustomButton } from "./components/Button/CustomButton";
 
 export const Home: React.FC = () => {
+  const handleClick = (): void => {
+    console.log("Click!");
+  }
+
   return (
     <div className="p-4">
       <div className="pb-8 text-5xl text-center">Next.js App</div>
       <div className="pb-8 text-center">
-        <CustomButton label="ボタン" />
+        <CustomButton label="ボタン" onClick={handleClick} />
       </div>
     </div>
   )
